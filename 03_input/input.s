159f2738abbaf09a33ae5e48fba4bf63c51400fb
enemyatgit <oliver.mail@web.de>
2018-12-23 21:04:08 +0100

    Added input

--- 03_input/input.s
+++ 03_input/input.s
@@ -8,28 +8,48 @@
 
 .text
 _start:
+    /* Write user prompt */
     movl  	$4, %eax	/* sys write 	*/
     movl  	$1, %ebx	/* fd		*/
-    movl  	$msg, %ecx	/* buffer	*/
-    movl  	$len, %edx	/* count	*/
+    movl  	$msg_get, %ecx	/* buffer	*/
+    movl  	$len_get, %edx	/* count	*/
     int   	$0x80		/* syscall	*/
 
+    /* Read input and store */
+    movl	$3,	%eax
+    movl	$1, 	%ebx
+    movl	$num,	%ecx
+    movl	$5,	%edx
+    int		$0x80
+
+    /* Write output message */
+    movl  	$4,	%eax	/* sys write 	*/
+    movl  	$1,	%ebx	/* fd		*/
+    movl  	$msg_put, %ecx	/* buffer	*/
+    movl  	$len_put, %edx	/* count	*/
+    int   	$0x80		/* syscall	*/
+
+    /* Write the stored number */
     movl  	$4, %eax	/* sys write 	*/
     movl  	$1, %ebx	/* fd		*/
-    movl  	$msg1, %ecx	/* buffer	*/
-    movl  	$len1, %edx	/* count	*/
+    movl  	$num, %ecx	/* buffer	*/
+    movl  	$5, %edx	/* count	*/
     int   	$0x80		/* syscall	*/
 
+    /* Exit */
     movl  	$1, %eax	/* sys_exit	*/
     movl  	$0, %ebx	/* error_code	*/
     int   	$0x80		/* syscall	*/
 
 .data
-msg:
-    .ascii  	"Hello, world!\n"
-    len =   	. - msg		/* Symbol "." refers to the current address 
+msg_get:
+    .ascii  	"Please enter a number:"
+    len_get =  	. - msg_get	/* Symbol "." refers to the current address 
 			         * Therefore len = current address - start of msg */
 
-msg1:
-    .ascii 	"This is the second line\n"
-    len1 =	. - msg1
+msg_put:
+    .ascii 	"You have entered:"
+    len_put =	. - msg_put
+
+.bss
+    .lcomm	num,	5	/* Reserve 5 byte buffer at local common section */
