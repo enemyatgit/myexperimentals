91eb570ba9d4bfc5ef72c5bfc72c942a3b5e888e
enemyatgit <46091022+enemyatgit@users.noreply.github.com>
2018-12-23 21:08:19 +0100

    Update input.s

--- 03_input/input.s
+++ 03_input/input.s
@@ -9,47 +9,47 @@
 .text
 _start:
     /* Write user prompt */
-    movl  	$4, %eax	/* sys write 	*/
-    movl  	$1, %ebx	/* fd		*/
-    movl  	$msg_get, %ecx	/* buffer	*/
-    movl  	$len_get, %edx	/* count	*/
-    int   	$0x80		/* syscall	*/
+    movl  	$4,			%eax	/* sys write 	*/
+    movl  	$1, 		%ebx	/* fd		*/
+    movl  	$msg_get, 	%ecx	/* buffer	*/
+    movl  	$len_get, 	%edx	/* count	*/
+    int   	$0x80				/* syscall	*/
 
-    /* Read input and store */
-    movl	$3,	%eax
-    movl	$1, 	%ebx
-    movl	$num,	%ecx
-    movl	$5,	%edx
+    /* Read input and store 					*/
+    movl	$3,			%eax
+    movl	$1, 		%ebx
+    movl	$num,		%ecx
+    movl	$5,			%edx
     int		$0x80
 
-    /* Write output message */
-    movl  	$4,	%eax	/* sys write 	*/
-    movl  	$1,	%ebx	/* fd		*/
-    movl  	$msg_put, %ecx	/* buffer	*/
-    movl  	$len_put, %edx	/* count	*/
-    int   	$0x80		/* syscall	*/
+    /* Write output message 					*/
+    movl  	$4,			%eax	/* sys write 	*/
+    movl  	$1,			%ebx	/* fd			*/
+    movl  	$msg_put, 	%ecx	/* buffer		*/
+    movl  	$len_put, 	%edx	/* count		*/
+    int   	$0x80				/* syscall		*/
 
-    /* Write the stored number */
-    movl  	$4, %eax	/* sys write 	*/
-    movl  	$1, %ebx	/* fd		*/
-    movl  	$num, %ecx	/* buffer	*/
-    movl  	$5, %edx	/* count	*/
-    int   	$0x80		/* syscall	*/
+    /* Write the stored number 					*/
+    movl  	$4, 		%eax	/* sys write 	*/
+    movl  	$1, 		%ebx	/* fd			*/
+    movl  	$num, 		%ecx	/* buffer		*/
+    movl  	$5, 		%edx	/* count		*/
+    int   	$0x80				/* syscall		*/
 
-    /* Exit */
-    movl  	$1, %eax	/* sys_exit	*/
-    movl  	$0, %ebx	/* error_code	*/
-    int   	$0x80		/* syscall	*/
+    /* Exit 									*/
+    movl  	$1, 		%eax	/* sys_exit		*/
+    movl  	$0, 		%ebx	/* error_code	*/
+    int   	$0x80				/* syscall		*/
 
 .data
 msg_get:
-    .ascii  	"Please enter a number:"
-    len_get =  	. - msg_get	/* Symbol "." refers to the current address 
-			         * Therefore len = current address - start of msg */
+    .ascii  			"Please enter a number:"
+    len_get =  			. - msg_get		/* Symbol "." refers to the current address 
+			         					 * Therefore len = current address - start of msg */
 
 msg_put:
-    .ascii 	"You have entered:"
-    len_put =	. - msg_put
+    .ascii 				"You have entered:"
+    len_put =			. - msg_put
 
 .bss
-    .lcomm	num,	5	/* Reserve 5 byte buffer at local common section */
+    .lcomm	num,		5				/* Reserve 5 byte buffer at local common section */
