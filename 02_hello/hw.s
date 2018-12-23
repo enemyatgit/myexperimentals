3d62b8305c34eb1f06de232b3fb6fb84f284e8ba
enemyatgit <oliver.mail@web.de>
2018-12-23 20:10:30 +0100

    Updated formatting

--- 02_hello/hw.s
+++ 02_hello/hw.s
@@ -8,17 +8,18 @@
 
 .text
 _start:
-    movl  $4, %eax		/* sys write 	*/
-    movl  $1, %ebx		/* fd		*/
-    movl  $msg, %ecx		/* buffer	*/
-    movl  $len, %edx		/* count	*/
-    int   $0x80			/* syscall	*/
+    movl  	$4, %eax	/* sys write 	*/
+    movl  	$1, %ebx	/* fd		*/
+    movl  	$msg, %ecx	/* buffer	*/
+    movl  	$len, %edx	/* count	*/
+    int   	$0x80		/* syscall	*/
 
-    movl  $1, %eax		/* sys_exit	*/
-    movl  $0, %ebx		/* error_code	*/
-    int   $0x80			/* syscall	*/
+    movl  	$1, %eax	/* sys_exit	*/
+    movl  	$0, %ebx	/* error_code	*/
+    int   	$0x80		/* syscall	*/
+
 .data
 msg:
-    .ascii  "Hello, world!\n"
-    len =   . - msg		/* Symbol "." refers to the current address 
+    .ascii  	"Hello, world!\n"
+    len =   	. - msg		/* Symbol "." refers to the current address 
 			         * Therefore len = current address - start of msg */
