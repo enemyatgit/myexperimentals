8b4ae0bb8111b67359a002c3314876fd7acef1bf
enemyatgit <oliver.mail@web.de>
2018-12-23 20:36:22 +0100

    Initial version

---
+++ 03_input/input.s
@@ -0,0 +1,35 @@
+/*
+ *    Second assembly program
+ *    This program uses Linux syscall
+ *    in order to write a string to the console
+ */
+
+.global	_start
+
+.text
+_start:
+    movl  	$4, %eax	/* sys write 	*/
+    movl  	$1, %ebx	/* fd		*/
+    movl  	$msg, %ecx	/* buffer	*/
+    movl  	$len, %edx	/* count	*/
+    int   	$0x80		/* syscall	*/
+
+    movl  	$4, %eax	/* sys write 	*/
+    movl  	$1, %ebx	/* fd		*/
+    movl  	$msg1, %ecx	/* buffer	*/
+    movl  	$len1, %edx	/* count	*/
+    int   	$0x80		/* syscall	*/
+
+    movl  	$1, %eax	/* sys_exit	*/
+    movl  	$0, %ebx	/* error_code	*/
+    int   	$0x80		/* syscall	*/
+
+.data
+msg:
+    .ascii  	"Hello, world!\n"
+    len =   	. - msg		/* Symbol "." refers to the current address 
+			         * Therefore len = current address - start of msg */
+
+msg1:
+    .ascii 	"This is the second line\n"
+    len1 =	. - msg1
